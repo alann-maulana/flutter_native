12a4deb3d44479cedf136519330b63bd3948eabf
alann-maulana <alann_it06@yahoo.co.id>
2021-07-31 19:16:36 +0700

    add ResponsiveScaffold

--- lib/src/native_scaffold.dart
+++ lib/src/native_scaffold.dart
@@ -81,3 +81,258 @@ class NativeScaffold extends BaseNativeStatelessWidget<
     );
   }
 }
+
+class ResponsiveScaffold extends StatelessWidget {
+  const ResponsiveScaffold({
+    Key? key,
+    this.scaffoldKey,
+    this.drawer,
+    this.endDrawer,
+    this.title,
+    this.centerTitle,
+    this.body,
+    this.trailing,
+    this.floatingActionButton,
+    this.menuIcon,
+    this.endIcon,
+    this.kTabletBreakpoint = 720.0,
+    this.kDesktopBreakpoint = 1280.0,
+    this.appBarElevation,
+  }) : super(key: key);
+
+  final Widget? drawer, endDrawer;
+
+  final Widget? title;
+
+  final bool? centerTitle;
+
+  final Widget? body;
+
+  final Widget? trailing;
+
+  final Widget? floatingActionButton;
+
+  final double kTabletBreakpoint;
+
+  final double kDesktopBreakpoint;
+
+  double get _drawerWidth => 304.0;
+
+  final IconData? menuIcon, endIcon;
+
+  final double? appBarElevation;
+
+  final Key? scaffoldKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        if (constraints.maxWidth >= kDesktopBreakpoint) {
+          return desktopLayout();
+        }
+        if (constraints.maxWidth >= kTabletBreakpoint) {
+          return tabletLayout();
+        }
+        return mobileLayout();
+      },
+    );
+  }
+
+  Widget desktopLayout() {
+    return material.Material(
+      child: Stack(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              if (drawer != null) ...[
+                SizedBox(
+                  width: _drawerWidth,
+                  child: material.Drawer(
+                    child: SafeArea(
+                      child: drawer!,
+                    ),
+                  ),
+                ),
+              ],
+              Expanded(
+                child: material.Scaffold(
+                  key: scaffoldKey,
+                  appBar: material.AppBar(
+                    elevation: appBarElevation,
+                    automaticallyImplyLeading: false,
+                    title: title,
+                    centerTitle: centerTitle,
+                    actions: <Widget>[
+                      if (trailing != null) ...[
+                        trailing!,
+                      ],
+                    ],
+                  ),
+                  body: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: body ?? Container(),
+                      ),
+                      if (endDrawer != null) ...[
+                        SizedBox(
+                          width: _drawerWidth,
+                          child: material.Drawer(
+                            elevation: 3.0,
+                            child: SafeArea(
+                              child: endDrawer!,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          if (floatingActionButton != null) ...[
+            Positioned(
+              top: 100.0,
+              left: _drawerWidth - 30,
+              child: floatingActionButton!,
+            )
+          ],
+        ],
+      ),
+    );
+  }
+
+  Widget tabletLayout() {
+    return material.Scaffold(
+      key: scaffoldKey,
+      drawer: drawer == null
+          ? null
+          : material.Drawer(
+              child: SafeArea(
+                child: drawer!,
+              ),
+            ),
+      appBar: material.AppBar(
+        elevation: appBarElevation,
+        automaticallyImplyLeading: false,
+        title: title,
+        leading: _MenuButton(iconData: menuIcon),
+        actions: <Widget>[
+          if (trailing != null) ...[
+            trailing!,
+          ],
+        ],
+      ),
+      body: SafeArea(
+        right: false,
+        bottom: false,
+        child: Stack(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: body ?? Container(),
+                ),
+                if (endDrawer != null) ...[
+                  SizedBox(
+                    width: _drawerWidth,
+                    child: material.Drawer(
+                      elevation: 3.0,
+                      child: SafeArea(
+                        child: endDrawer!,
+                      ),
+                    ),
+                  ),
+                ],
+              ],
+            ),
+            if (floatingActionButton != null) ...[
+              Positioned(
+                top: 10.0,
+                left: 10.0,
+                child: floatingActionButton!,
+              )
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget mobileLayout() {
+    return material.Scaffold(
+      key: scaffoldKey,
+      drawer: drawer == null
+          ? null
+          : material.Drawer(
+              child: SafeArea(
+                child: drawer!,
+              ),
+            ),
+      endDrawer: endDrawer == null
+          ? null
+          : material.Drawer(
+              child: SafeArea(
+                child: endDrawer!,
+              ),
+            ),
+      appBar: material.AppBar(
+        elevation: appBarElevation,
+        automaticallyImplyLeading: false,
+        leading: _MenuButton(iconData: menuIcon),
+        title: title,
+        actions: <Widget>[
+          if (trailing != null) ...[
+            trailing!,
+          ],
+          if (endDrawer != null) ...[
+            _OptionsButton(iconData: endIcon),
+          ]
+        ],
+      ),
+      body: body,
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation:
+          material.FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
+
+class _OptionsButton extends StatelessWidget {
+  const _OptionsButton({
+    Key? key,
+    required this.iconData,
+  }) : super(key: key);
+
+  final IconData? iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return material.IconButton(
+      icon: Icon(iconData ?? material.Icons.more_vert),
+      onPressed: () {
+        material.Scaffold.of(context).openEndDrawer();
+      },
+    );
+  }
+}
+
+class _MenuButton extends StatelessWidget {
+  const _MenuButton({
+    Key? key,
+    required this.iconData,
+  }) : super(key: key);
+
+  final IconData? iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return material.IconButton(
+      icon: Icon(iconData ?? material.Icons.menu),
+      onPressed: () {
+        material.Scaffold.of(context).openDrawer();
+      },
+    );
+  }
+}
